8e1f1ef12a6e39fd27112339e5dd3e6bdc2d7bbe
Jonathan Myers <myersjp1@rose-hulman.edu>
2021-08-26 04:18:36 -0500

    Prevent Crash Loss

--- TiltAngleOrganizer.py
+++ TiltAngleOrganizer.py
@@ -14,16 +14,18 @@ directory path.
 image data directory.
 """
 
-## Articles
+# Articles
 
 def TiltAngleOrganizer():
+    #%%
     import re
     import os
     #import subprocess as sp
-   
+    
     # Regular Expression:
-    regEx = '([a-zA-Z0-9-]*[_])?([0-9]+)[_]([-]?[0-9]+[\.][0-9]+)[_]([a-zA-Z0-9]+)[_]([0-9]+[\.][0-9]+[\.][0-9]+)'
-    patternFinder = re.compile(regEx)
+    regEx = ['([a-zA-Z0-9-]*[_])?','([0-9]+)[_]','([-]?[0-9]+[\.][0-9]+)[_]',
+             '([a-zA-Z0-9]+)[_]','([0-9]+[\.][0-9]+[\.][0-9]+)']
+    patternFinder = re.compile(''.join(regEx))
     
     # Acquire Files From Directory
     cwd = os.getcwd()
@@ -34,10 +36,25 @@ def TiltAngleOrganizer():
     # Mine Filename Metadata
     dataInfo = []
     for i in range(dataLen):
-        dataInfo.append(*patternFinder.findall(dataFiles[i]))
+        dataInfo.append(patternFinder.findall(dataFiles[i])[0])
     
-    angles = [val[2] for val in dataInfo]
-
+    # Sort Files On Angle
+    angles = [float(val[2]) for val in dataInfo]
+    sortedAngleIndex = sorted(range(dataLen),key=lambda x:angles[x])
+    sortedFiles = [dataFiles[index] for index in sortedAngleIndex]
+    
+    # Write To Text File For IMOD
+    imodInputFile = open("sortedAngles.txt", 'w')
+    imodInputFile.write(str(dataLen))
+    imodInputFile.write("\n")
+    for index in range(dataLen):
+        imodInputFile.write(sortedFiles[index])
+        imodInputFile.write("\n")
+    imodInputFile.close()
+    
+    # Control IMOD newstack Function
+    
+    #%%
 
 # If Code Independent, Run; If Code Imported, Do Not Run
 if __name__ == '__main__':
