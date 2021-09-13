292a02d5e5704158822855a4e2abc41acafe6833
Jonathan Myers <jmyers@morgridge.org>
2021-09-13 17:05:10 -0500

    BinVolume Program Commit

--- BinVolumeAutomator.py
+++ BinVolumeAutomator.py
@@ -13,5 +13,32 @@
 
 ## Articles
 
+def BinVolumeAutomator():
+    import sys
+    import re
+    import os
+    import subprocess as sp
+    
+    # Define Input Variables
+    inputPath = str(sys.argv[1])
+    if len(sys.argv) == 3:
+        binSelect = int(sys.argv[2])
+    else:
+        binSelect = 2
+    
+    # Regular Expression:
+    regEx = ['([a-zA-Z0-9-]*[_])?','([0-9]+)[_]','([a-zA-Z0-9]+)[_]','([0-9]+[\.][0-9]+[\.][0-9]+)']
+    patternFinder = re.compile(''.join(regEx))
+    
+    # Acquire Files From Directory
+    #dataDirectory = os.getcwd()
+    dataDirectory = os.chdir(inputPath)
+    dataFiles = os.listdir(dataDirectory)
+    dataLen = len(dataFiles)
+    
+    # Mine Filename Metadata
+    dataInfo = []
+    for i in range(dataLen):
+        dataInfo.append(patternFinder.findall(dataFiles[i])[0])
 
 # M02 End Program
