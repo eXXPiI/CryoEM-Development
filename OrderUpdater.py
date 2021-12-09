2fddc73cb88a1461eedb3c463e4a2b04e56959c2
Jonathan Myers <jmyers@morgridge.org>
2021-12-09 15:08:30 -0600

    Added OrderUpdater For Actual Tilt Angle Mutation

---
+++ OrderUpdater.py
@@ -0,0 +1,24 @@
+## Preamble
+"""
+# Program: OrderUpdater.py
+# Version: 0.0.1
+# Author: Jonathan Myers
+# Date Created: Thu Dec  9 15:04:48 2021
+# Date Modified: Dec 9 2021
+# Purpose: 
+# Imports: 
+# Inputs/Arguments: 
+# Outputs/Returns: 
+"""
+
+## Articles
+
+def OrderUpdater():
+    print(None)
+
+    
+# If Code Independent, Run; If Code Imported, Do Not Run
+if __name__ == '__main__':
+    OrderUpdater()
+
+# M02 End Program
