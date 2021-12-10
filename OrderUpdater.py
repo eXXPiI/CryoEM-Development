4e8aa8f285f85d568559d6a6a58214b2403632a5
Jonathan Myers <jmyers@morgridge.org>
2021-12-09 20:00:16 -0600

    General Updates

--- OrderUpdater.py
+++ OrderUpdater.py
@@ -13,12 +13,21 @@
 
 ## Articles
 
-def OrderUpdater():
-    print(None)
+def OrderUpdater(inputPath):
+    import os
+    import subprocess as sp
+    
+    # Acquire Subdirectories From Directory
+    dataDirectory = os.chdir(inputPath)
+    dataPartitions = os.listdir(dataDirectory)
 
     
 # If Code Independent, Run; If Code Imported, Do Not Run
 if __name__ == '__main__':
-    OrderUpdater()
+    import sys
+    # Define Input Variables
+    #inputPath = str(sys.argv[1])
+    inputPath = "/home/jmyers/Documents/testFolder/Unique"
+    OrderUpdater(inputPath)
 
 # M02 End Program
