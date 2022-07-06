93c5aaa89e2cc077dbd23a5ba3a075f963c94f1d
Jonathan Myers <jmyers@morgridge.org>
2022-07-06 16:39:04 -0500

    ModelStalker Improved Function

--- ModelStalker.py
+++ ModelStalker.py
@@ -4,7 +4,7 @@
 # Version: 0.0.1
 # Author: Jonathan Myers
 # Date Created: Tue Jul  5 16:35:28 2022
-# Date Modified: Jul 5 2022
+# Date Modified: Jul 6 2022
 # Purpose: Perform stalking on all models located within given directory.
 # Imports: sys, re (regular expression), os (operating system), and subprocess.
 # Inputs/Arguments: Tomogram dataset directory path. Works with additional 
@@ -21,8 +21,8 @@ def ModelStalker(inputPath):
     
     # Regular Expression And Parsing Format:
     # Filename Format: tilt<NavID>_Suffix.Extension
-    mainRegEx = ['tilt','([0-9]+)[_]?','([a-zA-Z0-9-_]*?)','(?:_s([0-9]*))?',
-             '[.](mod)']
+    mainRegEx = ['tilt','([0-9]+)[_]?','([a-zA-Z0-9-_]*?)',
+             '(head|tail|centroid)?','[.](mod)']
     mainPatternFinder = re.compile(''.join(mainRegEx))
     
     # Acquire Image Files From Directory
@@ -37,19 +37,32 @@ def ModelStalker(inputPath):
             if metaInfo[2] == '':
                 imageInfo.append(metaInfo)
                 imageFiles.append(file)
+            else:
+                os.remove(file)
         except IndexError:
             # No Computation Time Dedicated To Non-Scheme Files
             pass
     
-    # Control IMOD/PEET stalkInit Function
-    for file in imageFiles:
-        stalkCommand = " ".join(["stalkInit",file])
-        sp.run(stalkCommand,shell=True)
+    # Set Model Output Filenames
+    imageFileNumber = len(imageFiles)
+    nameScheme =  lambda navID,suffix,pointType: f"tilt{navID}_{suffix}_{pointType}.mod"
     
-    # Remove Overwrritten Files
-    os.remove("head.mod")
-    os.remove("tail.mod")
-    os.remove("centroid.mod")
+    outputModels = []
+    for i in range(imageFileNumber):
+        individualModels = []
+        individualModels.append(nameScheme(imageInfo[i][0],imageInfo[i][1],"head"))
+        individualModels.append(nameScheme(imageInfo[i][0],imageInfo[i][1],"tail"))
+        individualModels.append(nameScheme(imageInfo[i][0],imageInfo[i][1],"centroid"))
+        outputModels.append(individualModels)
+    
+    # Control IMOD/PEET stalkInit Function
+    for i in range(imageFileNumber):
+        stalkCommand = " ".join(["stalkInit",imageFiles[i]])
+        sp.run(stalkCommand,shell=True)
+        # Rename Main Model Outputs
+        os.rename("head.mod",outputModels[i][0])
+        os.rename("tail.mod",outputModels[i][1])
+        os.rename("centroid.mod",outputModels[i][2])
 
 
 # If Code Independent, Run; If Code Imported, Do Not Run
@@ -57,13 +70,13 @@ if __name__ == '__main__':
     import sys
     
     # Testing/Debugging Lines
-    """
+    #"""
     inputPath = "/home/jmyers/Documents/testFolder/TemplateProcessing"
     """
     
     # Define Input Variables
     inputPath = str(sys.argv[1])
-    
+    """
     # Main Script Run
     ModelStalker(inputPath)
 
